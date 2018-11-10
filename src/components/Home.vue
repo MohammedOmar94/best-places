32c1816c6d808c89193020e7536b7d033a8f9f74
Heights94 <mohammed_omar94@hotmail.co.uk>
2018-11-10 18:52:52 +0000

    Initial commit.

---
+++ src/components/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <div class="welcome--popup slide-up" v-if="hideMsg">
+      <h2 class="fade-in">{{ title }}</h2>
+      <span class="fade-in--slow">{{ caption }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import NavBar from './NavBar';
+
+export default {
+  name: 'Home',
+  components: { NavBar },
+  data() {
+    return {
+      hideMsg: true,
+      title: 'Welcome',
+      caption: 'Find the best place to be.',
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      // this.hideMsg = !this.hideMsg;
+    }, 2000);
+  },
+}
+</script>
+
+<style scoped>
+
+  /* The animation code */
+  @keyframes slide-up {
+      from { top: 100%; }
+      to { top: 30% }
+  }
+  @keyframes fade-in {
+      from { opacity: 0; }
+      to { opacity: 100 }
+  }
+  .welcome--popup {
+    width: 100%;
+    position: absolute;
+    top: 30%;
+    left: 50%;
+  }
+  .slide-up {
+    transform: translate(-50%);
+    animation-name: slide-up;
+    animation-duration: 1s;
+    
+  }
+  .fade-in {
+    animation-name: fade-in;
+    animation-duration: 1s;
+    animation-delay: 0.5s;
+    animation-fill-mode: both;
+  }
+  .fade-in--slow {
+    animation-name: fade-in;
+    animation-duration: 1s;
+    animation-delay: 1s;
+    animation-fill-mode: both;
+  }
+  h2 {
+    text-align: center;
+  }
+  span {
+    display: block;
+    text-align: center;
+    margin-top: 8px;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="menu">
+    <h1 class="header">Best Places</h1>
+    <div>
+      <a class="link">To Eat</a>
+      <a class="link">To Explore</a>
+      <a class="link">To Shop</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style scoped lang="scss">
+  .menu {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 0;
+    width: 100%;
+    background-color: rgb(225, 236, 255);
+  }
+  .header {
+    font-family: Calling Heart;
+    font-weight: normal;
+    font-size: 40px;
+    color: black;
+    margin-left: 16px;
+  }
+  .link {
+    cursor: pointer;
+    font-size: 14px;
+    padding: 8px 16px;
+    &:not(:last-child) {
+      border-right: 1px solid rgb(145, 145, 145);
+    }
+  }
+</style>
